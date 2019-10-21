c3f4dad9805df8f3fa13997970bfa3cd3e0a6900
Stephen Gream <stephen.gream@gmail.com>
2019-10-22 00:48:13 +1100

    Fixed up issues in the core code

--- tcp_proxy.py
+++ tcp_proxy.py
@@ -8,7 +8,7 @@ import threading
 class ProxyServer:
     def __init__(self, target_host, target_port, server_addr, server_port):
         self.__target_host = target_host
-        self.__target_post = target_port
+        self.__target_port = target_port
         self.__server_addr = server_addr
         self.__server_port = server_port
         self.__server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,16 +20,18 @@ class ProxyServer:
         self.__server_socket.bind((self.__server_addr, self.__server_port))
         self.__server_socket.listen(5)
         self.__reader_thread = threading.Thread(target=self.__reader_loop)
-        self.__reader_thread.run()
+        self.__reader_thread.start()
         try:
             while True:
                 client, addr = self.__server_socket.accept()
+                print("[*] Accepted connection from {}".format(addr))
                 target_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 try:
                     target_sock.connect((self.__target_host, self.__target_port))
                     self.__socket_pairs.append((client, target_sock))
-                except:
+                except Exception as e:
                     print("[!!] Could not connect to target machine")
+                    print(e)
                     client.shutdown(socket.SHUT_RDWR)
                     client.close()
         except KeyboardInterrupt:
@@ -38,6 +40,7 @@ class ProxyServer:
             print("[!!] Caught exception")
             print(e)
         finally:
+            self.__close = True
             self.__reader_thread.join()
             for (x,y) in self.__socket_pairs:
                 x.shutdown(socket.SHUT_RDWR)
@@ -49,7 +52,8 @@ class ProxyServer:
 
     def __reader_loop(self):
         while not self.__close:
-            (rs, ws, xs) = select.select([item for tempList in self.__socket_pairs for item in tempList], [], [], 0.1)
+            flatlist = [item for tempList in self.__socket_pairs for item in tempList]
+            (rs, ws, xs) = select.select(flatlist, [], [], 0.1)
             for s in rs:
                 matching_sock = None
                 pair = None
@@ -64,7 +68,7 @@ class ProxyServer:
                         print("[==>] Received message")
                         break
                         pair = (x,y)
-                bufffer = s.recv(4096)
+                buffer = s.recv(4096)
                 if len(buffer) == 0 or buffer == b'\xff\xf4\xff\xfd\x06':
                     self.__socket_pairs.remove(pair)
                     s.shutdown(socket.SHUT_RDWR)
@@ -72,7 +76,7 @@ class ProxyServer:
                     matching_sock.shutdown(socket.SHUT_RDWR)
                     matching_sock.close()
                     continue
-                matching_sock.send(bufffer)
+                matching_sock.send(buffer)
 
 
 def parse_args():
